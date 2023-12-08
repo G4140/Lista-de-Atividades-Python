ffbbc49d5ad583ea9398284da5a7d9a5a42ee17a
DiCaprio <152615917+G4140@users.noreply.github.com>
2023-12-08 11:35:46 -0300

    Create AtividadeClasse.py
    Primeira Atividade em Python com Classes

---
+++ Lista_de_Atividade_Python/AtividadeClasse.py
@@ -0,0 +1,29 @@
+class Relatorio:
+
+  def __init__(self, nome, notas, progresso, materias, Professor):
+    self.nome = nome
+    self.notas = notas
+    self.progresso = progresso
+    self.materias = materias
+    self.Professor = Professor
+
+
+class Professor:
+
+  def Relatorio(self):
+    print(f'Nome: {self.nome}')
+
+
+class Turma:
+
+  def __init__(self, nome, professor, alunos):
+    self.nome = nome
+    self.professor = professores
+    self.alunos = alunos
+
+
+class Aluno:
+
+  def __init__(self, nome, turma):
+    self.nome = nomes
+    self.turma = turmas
